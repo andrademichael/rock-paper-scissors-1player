f5a89926cda612870944caab68b12ad15e36ceed
Michael Andrade and Tracie Weitzman <michael@>
2016-11-16 11:30:04 -0800

    make rps method return true for all winning conditions

--- lib/rps.rb
+++ lib/rps.rb
@@ -2,6 +2,10 @@ class String
   define_method(:rps) do |opponent_choice|
     if self == "rock" && opponent_choice == "scissors"
       true
+    elsif self == "scissors" && opponent_choice == "paper"
+      true
+    elsif self == "paper" && opponent_choice == "rock"
+      true
     end
   end
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -6,4 +6,10 @@ describe("it will take the user's choice and play a round of Rock, Paper, Scisso
   it("returns true if rock is the object and scissors is the argument") do
     expect("rock".rps("scissors")).to(eq(true))
   end
+  it("returns true if scissors is the object and paper is the argument") do
+    expect("scissors".rps("paper")).to(eq(true))
+  end
+  it("returns true if paper is the object and rock is the argument") do
+    expect("paper".rps("rock")).to(eq(true))
+  end
 end
