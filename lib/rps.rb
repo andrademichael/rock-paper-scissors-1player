de9fed9346587263124372dce423e76365a770e3
Michael Andrade and Tracie Weitzman <michael@>
2016-11-16 11:17:55 -0800

    make rps method take input from two players (spec 1.1)

--- lib/rps.rb
+++ lib/rps.rb
@@ -1,9 +1,5 @@
 class String
-  define_method(:rps) do
-    #define variables
-
-    #act on self
-    self
-    #return something
+  define_method(:rps) do |opponent_choice|
+    return [self, opponent_choice]
   end
 end
--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -3,7 +3,8 @@ require('rspec')
 require('rps')
 
 describe("it will take the user's choice and play a round of Rock, Paper, Scissors") do
-  it("returns which weapon the user chose") do
-    expect("rock".rps()).to(eq("rock"))
+  it("returns which weapon each player chose") do
+    expect("rock".rps("scissors")).to(eq(["rock", "scissors"]))
   end
+    # it("returns true if rock is ")
 end
